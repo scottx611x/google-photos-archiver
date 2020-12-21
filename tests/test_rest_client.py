eeb1c47e02985a030b2021496d81adaa12ae1fad
Scott Ouellette <scottx611x@gmail.com>
2020-12-21 17:55:31 -0500

    Formatting

--- tests/test_rest_client.py
+++ tests/test_rest_client.py
@@ -1,7 +1,6 @@
 import pytest
 
-from src.rest_client import (GooglePhotosApiRestClient,
-                             GooglePhotosApiRestClientError)
+from src.rest_client import GooglePhotosApiRestClient, GooglePhotosApiRestClientError
 from tests.conftest import MockFailureResponse, MockSuccessResponse
 
 TEST_TOKEN = "TEST_TOKEN"
@@ -33,24 +32,20 @@ class TestGooglePhotosApiRestClient:
         )
 
     @pytest.mark.parametrize(
-        "params,expected_params", [
+        "params,expected_params",
+        [
+            (None, dict(pageSize=25)),
+            (dict(page_size=123), dict(pageSize=123)),
+            (dict(page_token="abc"), dict(pageSize=25, pageToken="abc")),
             (
-                None, dict(pageSize=25)
+                dict(page_size=123, page_token="abc"),
+                dict(pageSize=123, pageToken="abc"),
             ),
-            (
-                dict(page_size=123), dict(pageSize=123)
-            ),
-            (
-                dict(page_token="abc"), dict(pageSize=25, pageToken="abc")
-            ),
-            (
-                dict(page_size=123, page_token="abc"), dict(pageSize=123, pageToken="abc")
-            ),
-
-
-        ]
+        ],
     )
-    def test_get_media_items_params(self, mocker, google_photos_api_rest_client, params, expected_params):
+    def test_get_media_items_params(
+        self, mocker, google_photos_api_rest_client, params, expected_params
+    ):
         mock_get = mocker.patch(
             "src.rest_client.requests.get",
             return_value=MockSuccessResponse(),
@@ -58,7 +53,9 @@ class TestGooglePhotosApiRestClient:
         if params is None:
             get_media_items_response = google_photos_api_rest_client.get_media_items()
         else:
-            get_media_items_response = google_photos_api_rest_client.get_media_items(**params)
+            get_media_items_response = google_photos_api_rest_client.get_media_items(
+                **params
+            )
         assert get_media_items_response.ok
         mock_get.assert_called_with(
             "https://photoslibrary.googleapis.com/v1/mediaItems",
