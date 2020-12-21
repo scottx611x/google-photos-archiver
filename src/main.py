8397a11e510014a70cfca171d54c28db4375c215
Scott Ouellette <scottx611x@gmail.com>
2020-12-21 17:17:39 -0500

    Fix import

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
-from src.GooglePhotosRESTClient import GooglePhotosApiRestClient
-
+from src.rest_client import GooglePhotosApiRestClient
+from src.oauth_handler import GoogleOauthHandler
 
 if __name__ == "__main__":
     google_photos_api_rest_client = GooglePhotosApiRestClient(
-        Path("../client_secret.json")
+        GoogleOauthHandler(Path("../client_secret.json"))
     )
+    print(google_photos_api_rest_client.get_media_items().json())
