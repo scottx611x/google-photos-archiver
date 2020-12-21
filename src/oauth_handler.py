e589f1e8751dc42a2883803e26faf8d297100cbe
Scott Ouellette <scottx611x@gmail.com>
2020-12-21 18:34:48 -0500

    Add tests for GoogleOauthHandler

--- src/oauth_handler.py
+++ src/oauth_handler.py
@@ -16,15 +16,17 @@ class GoogleOauthHandler:
     def __init__(
         self,
         client_secret_file_path: Path,
+        refresh_token_path: Path
     ):
         self._authorization_url = "https://www.googleapis.com/oauth2/v4/token"
         self._client_secret_file_path = client_secret_file_path
+        self._refresh_token_path = refresh_token_path
         self._flow: InstalledAppFlow = self._get_flow()
         self._client_config: Dict[str, Any] = self._flow.client_config
 
-        refresh_token = self._read_refresh_token()
+        refresh_token = self.read_refresh_token()
         if refresh_token:
-            self.token = self.refresh_token(refresh_token).json()["access_token"]
+            self.token = self.refresh_existing_token(refresh_token).json()["access_token"]
         else:
             self.token = self._get_token()
 
@@ -45,18 +47,18 @@ class GoogleOauthHandler:
             scopes=["https://www.googleapis.com/auth/photoslibrary.readonly"],
         )
 
-    def _read_refresh_token(self) -> Optional[str]:
+    def _write_refresh_token(self, credentials: GoogleOauthCredentials):
+        with self._refresh_token_path.open("w") as f:
+            f.write(credentials.refresh_token)
+
+    def read_refresh_token(self) -> Optional[str]:
         try:
-            with open("../refresh_token") as f:
+            with self._refresh_token_path.open() as f:
                 return f.read()
         except FileNotFoundError:
             return None
 
-    def _write_refresh_token(self, credentials: GoogleOauthCredentials):
-        with open("../refresh_token", "w") as f:
-            f.write(credentials.refresh_token)
-
-    def refresh_token(self, refresh_token: str) -> requests.Response:
+    def refresh_existing_token(self, refresh_token: str) -> requests.Response:
         params = {
             "grant_type": "refresh_token",
             "client_id": self._client_config["client_id"],
