8baca9e6cd9f50892359eee14af3f48a9bdb1d88
Scott Ouellette <scottx611x@gmail.com>
2020-12-21 18:36:34 -0500

    Fix bad ref

--- src/main.py
+++ src/main.py
@@ -5,6 +5,6 @@ from src.rest_client import GooglePhotosApiRestClient
 
 if __name__ == "__main__":
     google_photos_api_rest_client = GooglePhotosApiRestClient(
-        GoogleOauthHandler(Path("../client_secret.json"))
+        GoogleOauthHandler(Path("../client_secret.json"), Path("../refresh_token"))
     )
     print(google_photos_api_rest_client.get_media_items().json())
