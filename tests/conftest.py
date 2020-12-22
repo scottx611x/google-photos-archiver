1f3eb3efa5d4f955d5f2b57bf6ef22e679f59506
Scott Ouellette <5629547+scottx611x@users.noreply.github.com>
2020-12-22 16:30:06 -0500

    Implement GooglePhotosApiRestClient.get_media_items_paginated (#4)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,78 @@
 import json
 
+import pytest
 from pytest_socket import disable_socket
 from requests import Response
+
+from src.media_item import VideoProcessingStatus
 
 
 def pytest_runtest_setup():
     disable_socket()
+
+
+TEST_ID = "abc123"
+TEST_URL = "http://www.example.com"
+TEST_PHOTO_MIMETYPE = "image/jpeg"
+TEST_PHOTO_FILENAME = "test.jpg"
+TEST_VIDEO_MIMETYPE = "video/mp4"
+TEST_VIDEO_FILENAME = "test.mp4"
+TEST_CREATION_TIME = "2020-12-22T01:23:45Z"
+TEST_WIDTH = "1024"
+TEST_HEIGHT = "2048"
+TEST_CAMERA_MAKE = "TEST_CAMERA_MAKE"
+TEST_CAMERA_MODEL = "TEST_CAMERA_MODEL"
+TEST_FOCAL_LENGTH = 1.23
+TEST_APERTURE_F_NUMBER = 1.23
+TEST_ISO_EQUIVALENT = 123
+
+TEST_FPS = 30
+TEST_VIDEO_PROCESSING_STATUS = VideoProcessingStatus.READY.value
+
+
+@pytest.fixture()
+def test_photo_media_item():
+    return {
+        "id": TEST_ID,
+        "productUrl": TEST_URL,
+        "baseUrl": TEST_URL,
+        "mimeType": TEST_PHOTO_MIMETYPE,
+        "filename": TEST_PHOTO_FILENAME,
+        "mediaMetadata": {
+            "creationTime": TEST_CREATION_TIME,
+            "width": TEST_WIDTH,
+            "height": TEST_HEIGHT,
+            "photo": {
+                "cameraMake": TEST_CAMERA_MAKE,
+                "cameraModel": TEST_CAMERA_MODEL,
+                "focalLength": TEST_FOCAL_LENGTH,
+                "apertureFNumber": TEST_APERTURE_F_NUMBER,
+                "isoEquivalent": TEST_ISO_EQUIVALENT,
+            },
+        },
+    }
+
+
+@pytest.fixture()
+def test_video_media_item():
+    return {
+        "id": TEST_ID,
+        "productUrl": TEST_URL,
+        "baseUrl": TEST_URL,
+        "mimeType": TEST_VIDEO_MIMETYPE,
+        "filename": TEST_VIDEO_FILENAME,
+        "mediaMetadata": {
+            "creationTime": TEST_CREATION_TIME,
+            "width": TEST_WIDTH,
+            "height": TEST_HEIGHT,
+            "video": {
+                "cameraMake": TEST_CAMERA_MAKE,
+                "cameraModel": TEST_CAMERA_MODEL,
+                "fps": TEST_FPS,
+                "status": TEST_VIDEO_PROCESSING_STATUS,
+            },
+        },
+    }
 
 
 class MockResponse(Response):
--- tests/test_media_item.py
+++ tests/test_media_item.py
@@ -1,78 +1,29 @@
-from typing import Any, Dict
-
-import pytest
-
 from src.media_item import (
     MediaItem,
     PhotoMediaMetadata,
     PhotoMetadata,
     VideoMediaMetadata,
     VideoMetadata,
-    VideoProcessingStatus,
     create_media_item,
 )
-
-TEST_ID = "abc123"
-TEST_URL = "http://www.example.com"
-TEST_MIMETYPE = "image/jpeg"
-TEST_FILENAME = "test.jpg"
-TEST_CREATION_TIME = "2020-12-22T01:23:45Z"
-TEST_WIDTH = "1024"
-TEST_HEIGHT = "2048"
-TEST_CAMERA_MAKE = "TEST_CAMERA_MAKE"
-TEST_CAMERA_MODEL = "TEST_CAMERA_MODEL"
-TEST_FOCAL_LENGTH = 1.23
-TEST_APERTURE_F_NUMBER = 1.23
-TEST_ISO_EQUIVALENT = 123
-
-TEST_FPS = 30
-TEST_VIDEO_PROCESSING_STATUS = VideoProcessingStatus.READY
-
-# pylint: disable=redefined-outer-name
-
-
-@pytest.fixture()
-def test_media_item():
-    return {
-        "id": TEST_ID,
-        "productUrl": TEST_URL,
-        "baseUrl": TEST_URL,
-        "mimeType": TEST_MIMETYPE,
-        "filename": TEST_FILENAME,
-    }
-
-
-@pytest.fixture()
-def test_photo_media_item(test_media_item):
-    test_media_item["mediaMetadata"] = {
-        "creationTime": TEST_CREATION_TIME,
-        "width": TEST_WIDTH,
-        "height": TEST_HEIGHT,
-        "photo": {
-            "cameraMake": TEST_CAMERA_MAKE,
-            "cameraModel": TEST_CAMERA_MODEL,
-            "focalLength": TEST_FOCAL_LENGTH,
-            "apertureFNumber": TEST_APERTURE_F_NUMBER,
-            "isoEquivalent": TEST_ISO_EQUIVALENT,
-        },
-    }
-    return test_media_item
-
-
-@pytest.fixture()
-def test_video_media_item(test_media_item):
-    test_media_item["mediaMetadata"]: Dict[str, Any] = {
-        "creationTime": TEST_CREATION_TIME,
-        "width": TEST_WIDTH,
-        "height": TEST_HEIGHT,
-        "video": {
-            "cameraMake": TEST_CAMERA_MAKE,
-            "cameraModel": TEST_CAMERA_MODEL,
-            "fps": TEST_FPS,
-            "status": TEST_VIDEO_PROCESSING_STATUS,
-        },
-    }
-    return test_media_item
+from tests.conftest import (
+    TEST_APERTURE_F_NUMBER,
+    TEST_CAMERA_MAKE,
+    TEST_CAMERA_MODEL,
+    TEST_CREATION_TIME,
+    TEST_FOCAL_LENGTH,
+    TEST_FPS,
+    TEST_HEIGHT,
+    TEST_ID,
+    TEST_ISO_EQUIVALENT,
+    TEST_PHOTO_FILENAME,
+    TEST_PHOTO_MIMETYPE,
+    TEST_URL,
+    TEST_VIDEO_FILENAME,
+    TEST_VIDEO_MIMETYPE,
+    TEST_VIDEO_PROCESSING_STATUS,
+    TEST_WIDTH,
+)
 
 
 def test_create_photo_media_item(test_photo_media_item):
@@ -81,7 +32,7 @@ def test_create_photo_media_item(test_photo_media_item):
         id=TEST_ID,
         productUrl=TEST_URL,
         baseUrl=TEST_URL,
-        mimeType=TEST_MIMETYPE,
+        mimeType=TEST_PHOTO_MIMETYPE,
         mediaMetadata=PhotoMediaMetadata(
             creationTime=TEST_CREATION_TIME,
             width=TEST_WIDTH,
@@ -94,7 +45,7 @@ def test_create_photo_media_item(test_photo_media_item):
                 isoEquivalent=TEST_ISO_EQUIVALENT,
             ),
         ),
-        filename=TEST_FILENAME,
+        filename=TEST_PHOTO_FILENAME,
     )
     assert photo_media_item.downloadUrl == photo_media_item.baseUrl + "=d"
 
@@ -105,7 +56,7 @@ def test_create_video_media_item(test_video_media_item):
         id=TEST_ID,
         productUrl=TEST_URL,
         baseUrl=TEST_URL,
-        mimeType=TEST_MIMETYPE,
+        mimeType=TEST_VIDEO_MIMETYPE,
         mediaMetadata=VideoMediaMetadata(
             creationTime=TEST_CREATION_TIME,
             width=TEST_WIDTH,
@@ -117,6 +68,6 @@ def test_create_video_media_item(test_video_media_item):
                 status=TEST_VIDEO_PROCESSING_STATUS,
             ),
         ),
-        filename=TEST_FILENAME,
+        filename=TEST_VIDEO_FILENAME,
     )
     assert video_media_item.downloadUrl == video_media_item.baseUrl + "=dv"
