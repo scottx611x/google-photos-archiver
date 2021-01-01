3a642bd7c824cd04896a012910fb683a923e794b
Scott Ouellette <scottx611x@gmail.com>
2021-01-01 14:07:43 -0500

    Dry up tests

--- tests/test_media_item_archiver.py
+++ tests/test_media_item_archiver.py
@@ -1,33 +1,42 @@
 from pathlib import Path
+from typing import List
+
+import pytest
 
 from src.archivers import DiskArchiver
+from src.media_item import MediaItem
 from src.media_item_archiver import MediaItemArchiver, get_new_media_item_archivals
 from tests.conftest import MockSuccessResponse
+
+TEST_MEDIA_CONTENT = bytes("I'm a test photo or a video!", "utf-8")
+
+
+@pytest.fixture()
+def _test_media_items(
+    mocker, test_photo_media_item, test_video_media_item
+) -> List[MediaItem]:
+    media_items = [test_photo_media_item, test_video_media_item]
+
+    mocker.patch(
+        "src.rest_client.requests.get",
+        side_effect=[
+            MockSuccessResponse(TEST_MEDIA_CONTENT),
+            MockSuccessResponse(TEST_MEDIA_CONTENT),
+        ],
+    )
+    return media_items
 
 
 class TestMediaItemArchiver:
     # pylint: disable=too-many-arguments
     def test_start_with_disk_archiver(
         self,
-        mocker,
-        test_photo_media_item,
-        test_video_media_item,
+        _test_media_items,
         test_media_item_recorder,
         tmp_path,
     ):
-        media_items = [test_photo_media_item, test_video_media_item]
-        fake_media_content = bytes("I'm a test photo or a video!", "utf-8")
-
-        mocker.patch(
-            "src.rest_client.requests.get",
-            side_effect=[
-                MockSuccessResponse(fake_media_content),
-                MockSuccessResponse(fake_media_content),
-            ],
-        )
-
         completed_media_item_archivals = MediaItemArchiver(
-            media_items=media_items,
+            media_items=_test_media_items,
             archiver=DiskArchiver(
                 download_path=tmp_path, recorder=test_media_item_recorder
             ),
@@ -35,7 +44,7 @@ class TestMediaItemArchiver:
 
         assert get_new_media_item_archivals(completed_media_item_archivals) == 2
 
-        for media_item in media_items:
+        for media_item in _test_media_items:
             assert test_media_item_recorder.lookup(media_item) is True
             with Path(
                 tmp_path,
@@ -44,30 +53,16 @@ class TestMediaItemArchiver:
                 str(media_item.creationTime.day),
                 media_item.filename,
             ).open("rb") as f:
-                assert f.read() == fake_media_content
+                assert f.read() == TEST_MEDIA_CONTENT
 
 
 def test_get_new_media_item_archivals(
-    mocker,
+    _test_media_items,
     test_media_item_recorder,
-    test_photo_media_item,
-    test_video_media_item,
     tmp_path,
 ):
-    media_items = [test_photo_media_item, test_video_media_item]
-
-    fake_media_content = bytes("I'm a test photo or a video!", "utf-8")
-
-    mocker.patch(
-        "src.rest_client.requests.get",
-        side_effect=[
-            MockSuccessResponse(fake_media_content),
-            MockSuccessResponse(fake_media_content),
-        ],
-    )
-
     completed_media_item_archivals = MediaItemArchiver(
-        media_items=media_items,
+        media_items=_test_media_items,
         archiver=DiskArchiver(
             download_path=tmp_path, recorder=test_media_item_recorder
         ),
